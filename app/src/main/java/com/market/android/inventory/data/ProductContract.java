3d8c95a72c9b2a320a80b45fcfe355ae8aef28ee
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2017-07-18 01:19:29 +0300

    Define ProductContract PROJECTION.

--- app/src/main/java/com/market/android/inventory/data/ProductContract.java
+++ app/src/main/java/com/market/android/inventory/data/ProductContract.java
@@ -15,10 +15,12 @@ public final class ProductContract {
     public static final class ProductEntry implements BaseColumns {
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
         public final static String TABLE_NAME = "products";
-        public final static String _ID = BaseColumns._ID;
+        final static String _ID = BaseColumns._ID;
         public final static String COLUMN_PRODUCT_NAME = "name";
         public final static String COLUMN_PRODUCT_PRICE = "price";
         public final static String COLUMN_PRODUCT_SUPPLIER_MAIL = "supplier_mail";
         public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public final static String[] PROJECTION = new String[]{_ID, COLUMN_PRODUCT_NAME,
+                COLUMN_PRODUCT_PRICE, COLUMN_PRODUCT_SUPPLIER_MAIL, COLUMN_PRODUCT_QUANTITY};
     }
 }
