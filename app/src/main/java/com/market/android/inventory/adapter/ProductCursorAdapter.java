c29c72c6cb80f1c909f439fc38a02ffd10d0d7e3
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2017-07-19 23:47:43 +0300

    Implement sale button.

--- app/src/main/java/com/market/android/inventory/adapter/ProductCursorAdapter.java
+++ app/src/main/java/com/market/android/inventory/adapter/ProductCursorAdapter.java
@@ -1,15 +1,23 @@
 package com.market.android.inventory.adapter;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 import com.market.android.inventory.R;
 import com.market.android.inventory.data.ProductContract.ProductEntry;
+import com.market.android.inventory.model.Product;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class ProductCursorAdapter extends CursorAdapter {
 
@@ -22,22 +30,40 @@ public class ProductCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        // Inflate a list item view using the layout specified in list_item.xml
         return LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false);
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, Context context, final Cursor cursor) {
+        final Product product = ProductEntry.getProductFromCursor(cursor);
+
         TextView productName = (TextView) view.findViewById(R.id.product_name);
         TextView productPrice = (TextView) view.findViewById(R.id.product_price);
-        TextView productQuatity = (TextView) view.findViewById(R.id.product_quantity);
+        TextView productQuantity = (TextView) view.findViewById(R.id.product_quantity);
 
-        int nameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
-        int priceColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
-        int quantityColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+        productName.setText(product.getName());
+        productPrice.setText(mContext.getString(R.string.dollar) + product.getPrice());
+        productQuantity.setText(Integer.toString(product.getQuantity()));
 
-        productName.setText(cursor.getString(nameColumnIndex));
-        productPrice.setText(mContext.getString(R.string.dollar) + cursor.getInt(priceColumnIndex));
-        productQuatity.setText(Integer.toString(cursor.getInt(quantityColumnIndex)));
+        Button saleButton = (Button) view.findViewById(R.id.sale_button);
+        if (product.getQuantity() == 0) {
+            saleButton.setVisibility(INVISIBLE);
+        } else {
+            final int id = cursor.getInt(cursor.getColumnIndex(ProductEntry._ID));
+            saleButton.setVisibility(VISIBLE);
+            saleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri currentProductUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(ProductEntry.COLUMN_PRODUCT_NAME, product.getName());
+                    contentValues.put(ProductEntry.COLUMN_PRODUCT_PRICE, product.getPrice());
+                    contentValues.put(ProductEntry.COLUMN_PRODUCT_SUPPLIER_MAIL, product.getSupplierMail());
+                    contentValues.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, product.getQuantity() - 1);
+                    mContext.getContentResolver().update(currentProductUri, contentValues, null, null);
+                }
+            });
+        }
     }
+
 }
