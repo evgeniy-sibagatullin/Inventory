980f58b2269a5936d42b2fae6ab4fbe8135ad407
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2017-07-21 00:32:25 +0300

    Add image uri string column to DB.

--- app/src/main/java/com/market/android/inventory/data/ProductDbHelper.java
+++ app/src/main/java/com/market/android/inventory/data/ProductDbHelper.java
@@ -6,25 +6,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.market.android.inventory.data.ProductContract.ProductEntry;
 
-public final class ProductDbHelper extends SQLiteOpenHelper {
+final class ProductDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "inventory.db";
     private static final int DATABASE_VERSION = 1;
 
-    public ProductDbHelper(Context context) {
+    ProductDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + ProductEntry.TABLE_NAME + " ("
+        String SQL_CREATE_PRODUCTS_TABLE = "CREATE TABLE " + ProductEntry.TABLE_NAME + " ("
                 + ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
                 + ProductEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL, "
                 + ProductEntry.COLUMN_PRODUCT_SUPPLIER_MAIL + " TEXT NOT NULL, "
-                + ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL DEFAULT 0);";
+                + ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL DEFAULT 0, "
+                + ProductEntry.COLUMN_PRODUCT_IMAGE_URI_STRING + " TEXT NOT NULL);";
 
-        sqLiteDatabase.execSQL(SQL_CREATE_PETS_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_PRODUCTS_TABLE);
     }
 
     @Override
